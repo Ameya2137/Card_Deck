804364c7aa4aaf443b944340cb73c24942e95c8d
Ameya Tipnis <164353861+Ameya2137@users.noreply.github.com>
2025-03-05 18:28:57 +0530

    Update Deck.java by adding method for create deck

--- Deck.java
+++ Deck.java
@@ -10,3 +10,19 @@ class Deck {
     public Deck() {
         this.deck = new ArrayList<>();
     }
+
+    // a) Create the deck of 52 cards
+    public void createDeck() {
+        String[] ranks = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+        String[] suits = {"Clubs", "Spades", "Diamonds", "Hearts"};
+
+        deck.clear(); // Clear the deck before creating a new one
+
+        // Iterating through each rank and then each suit to add a new card
+        for (String rank : ranks) {
+            for (String suit : suits) {
+                deck.add(new Card(rank, suit));
+            }
+        }
+        System.out.println("Deck successfully created!");
+    }
