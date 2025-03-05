c148067029b6fe0ebf96ca0536883cbbf4d20ff4
Ameya Tipnis <164353861+Ameya2137@users.noreply.github.com>
2025-03-05 17:55:07 +0530

    Create Main.java
    Defining Main class

---
+++ Main.java
@@ -0,0 +1,14 @@
+// Name: Ameya Tipnis
+// PRN: 23070126012
+// Batch: 2023-27 (A1)
+
+// Main.java
+
+import java.util.*; // Importing all java utilities 
+
+// Defining Main class
+class Main {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in); // Creates a Scanner object for user input
+        Deck deck = new Deck(); // Creates an instance of the Deck class
+        int choice;
